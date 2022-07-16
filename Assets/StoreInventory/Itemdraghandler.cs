29d9de817d0fc6bb88b807f18edde5b05cb1cd5e
Zirui Gan <GANZB@BC.EDU>
2022-07-17 02:12:27 +0800

    draghandler update

---
+++ Assets/StoreInventory/Itemdraghandler.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Itemdraghandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+
+    public Transform originalparent;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        originalparent = transform.parent;
+        transform.SetParent(transform.parent.parent);
+        transform.position = eventData.position;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+        
+        /*Debug.Log(eventData.pointerCurrentRaycast.gameObject.name);*/
+        
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+    
+        if (eventData.pointerCurrentRaycast.gameObject.name == "Image")
+        {
+            Debug.Log("working");
+            transform.SetParent(eventData.pointerCurrentRaycast.gameObject.transform.parent.parent);
+            transform.position = eventData.pointerCurrentRaycast.gameObject.transform.parent.parent.position;
+            eventData.pointerCurrentRaycast.gameObject.transform.parent.position = originalparent.position;
+            eventData.pointerCurrentRaycast.gameObject.transform.parent.SetParent(originalparent);
+            GetComponent<CanvasGroup>().blocksRaycasts = true;
+            return;
+        }
+
+       
+    }
+
+    
+}
